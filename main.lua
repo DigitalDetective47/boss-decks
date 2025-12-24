41607a1019f49562a8595233d97ac4bb03f7e624
DigitalDetective47 <ninji2701@gmail.com>
2025-12-23 23:43:28 -0500

    remove blind attribute from decks

--- main.lua
+++ main.lua
@@ -17,33 +17,39 @@ SMODS.Atlas {
     py = 95,
 }
 
+---get the key of the blind corresponding to this boss deck
+---@param deck SMODS.Back
+---@return string
+local function get_blind_key(deck)
+    return SMODS.Blind.class_prefix .. "_" .. deck.original_key
+end
 ---template implementation of `locked_loc_vars`
 ---@param self SMODS.Back
 ---@param info_queue table[]
 ---@param card Card
 ---@return { vars: [string, integer] }
 local function locked_loc_vars(self, info_queue, card)
-    return { vars = { localize { type = "name_text", set = "Blind", key = self.blind }, 7 } }
+    return { vars = { localize { type = "name_text", set = "Blind", key = get_blind_key(self) }, 7 } }
 end
 ---template implementation of `check_for_unlock`
 ---@param self SMODS.Back
 ---@param args table
 ---@return boolean
 local function check_for_unlock(self, args)
-    return args.type == "round_win" and G.GAME.round_resets.ante >= 7 and G.GAME.blind.config.blind.key == self.blind
+    return args.type == "round_win" and G.GAME.round_resets.ante >= 7 and
+    G.GAME.blind.config.blind.key == get_blind_key(self)
 end
 ---template implementation of `apply` that bans the deck's corresponding blind
 ---@param self SMODS.Back
 ---@param deck Back
 local function apply(self, deck)
-    G.GAME.banned_keys[self.blind] = true
+    G.GAME.banned_keys[get_blind_key(self)] = true
 end
 
 SMODS.Back {
     key = "serpent",
     atlas = "decks",
     pos = { x = 1, y = 2 },
-    blind = "bl_serpent",
     unlocked = false,
     locked_loc_vars = locked_loc_vars,
     check_for_unlock = check_for_unlock,
